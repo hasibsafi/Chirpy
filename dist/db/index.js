0195b339ef24bbde539f5d521dad0e3fa7d72660
Abdul Safi <abdul-hasib.safi.942@my.csun.edu>
2026-02-19 19:04:45 -0800

    initial commit

--- dist/db/index.js
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema.js";
-import { config } from "../config.js";
-const conn = postgres(config.db.dbURL);
-export const db = drizzle(conn, { schema });
